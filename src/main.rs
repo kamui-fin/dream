728bf038a6cde27fae130351eaca942b09ddb695
Kamui <fin-kamui@pm.me>
2024-11-01 17:04:23 -0500

    feat: implement find_peers and announce_peer

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use clap::Parser;
 use local_ip_address::local_ip;
+use rand::rngs::OsRng;
 use rand::Rng;
+use rand::RngCore;
 use serde::{Deserialize, Serialize};
 use sha1::{Digest, Sha1};
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::collections::LinkedList;
 use std::env;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::time::Duration;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::io::BufReader;
+use tokio::net::TcpListener;
 use tokio::net::UdpSocket;
+use tokio::time::sleep;
 
 // Number of bits for our IDs
 const NUM_BITS: usize = 6;
@@ -27,7 +38,21 @@ struct Node {
 }
 
 impl Node {
-    fn get_compact_format(&self) -> String {
+    fn get_peer_compact_format(&self) -> String {
+        let mut compact_info = [0u8; 6];
+
+        if let IpAddr::V4(v4_addr) = self.ip {
+            let ip = v4_addr.to_bits().to_le_bytes();
+            compact_info[0..4].copy_from_slice(&ip);
+        }
+
+        let port = self.port.to_le_bytes();
+        compact_info[4..6].copy_from_slice(&port);
+
+        format!("{:?}", compact_info)
+    }
+
+    fn get_node_compact_format(&self) -> String {
         let mut compact_info = [0u8; 7];
         compact_info[0] = self.id as u8;
 
@@ -41,6 +66,12 @@ impl Node {
 
         format!("{:?}", compact_info)
     }
+}
+
+fn gen_secret() -> [u8; 16] {
+    let mut secret = [0u8; 16];
+    OsRng.fill_bytes(&mut secret);
+    secret
 }
 
 // infohash of torrent = key id
@@ -63,31 +94,23 @@ impl Node {
 //    -> nodes that are able to receive queries from other nodes dno't need to refresh buckets often
 //    -> but nodes that can't need to refresh periodically  so good nodes are available when DHT is needed
 struct RoutingTable {
-    my_id: u32,
+    my_node: Node,
+    secret: Arc<Mutex<[u8; 16]>>,
     // array of linked lists with NUM_BITS elements
     buckets: Vec<LinkedList<Node>>,
 }
 
-struct PeerStore {}
-
-impl PeerStore {
-    fn new() -> Self {
-        Self {
-            store: HashMap::new(),
-        }
-    }
-}
-
 impl RoutingTable {
-    fn new(my_id: u32) -> Self {
+    fn new(my_node: Node, secret: Arc<Mutex<[u8; 16]>>) -> Self {
         Self {
-            my_id,
+            my_node,
+            secret,
             buckets: Vec::with_capacity(NUM_BITS),
         }
     }
 
     fn find_bucket_idx(&self, node_id: u32) -> u32 {
-        let xor_result = node_id ^ self.my_id;
+        let xor_result = node_id ^ self.my_node.id;
         return xor_result.leading_zeros() - ((32 - NUM_BITS) as u32);
     }
 
@@ -151,7 +174,15 @@ mod tests {
 
     #[test]
     fn test_bucket_finder() {
-        let rt = RoutingTable::new(0);
+        let secret = Arc::new(Mutex::new([0; 16]));
+        let rt = RoutingTable::new(
+            Node {
+                id: 0,
+                ip: IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+                port: 0,
+            },
+            secret,
+        );
         assert_eq!(rt.find_bucket_idx(0b001101), 2);
         assert_eq!(rt.find_bucket_idx(0b000001), 5);
     }
@@ -257,7 +288,7 @@ async fn handle_krpc_call(
     match query.q.as_str() {
         "ping" => {
             let mut return_values = HashMap::new();
-            return_values.insert("id".into(), routing_table.my_id.to_string());
+            return_values.insert("id".into(), routing_table.my_node.id.to_string());
 
             let response = KrpcSuccessResponse {
                 y: "r".into(),
@@ -268,8 +299,6 @@ async fn handle_krpc_call(
             socket.send_to(&response, addr).await.unwrap();
         }
         "find_node" => {
-            let mut k_closest_nodes = vec![];
-
             let source_id = query.a.get("id").unwrap().parse().unwrap();
             let source_node = Node {
                 id: source_id,
@@ -278,41 +307,10 @@ async fn handle_krpc_call(
             };
             let target_node_id = query.a.get("target").unwrap().parse().unwrap();
 
-            // 1. update source_id into routing table
-            routing_table.upsert_node(source_node);
-            // 2. find the initial k-bucket
-            let mut bucket_idx = routing_table.find_bucket_idx(target_node_id);
-            // 2.5. Check if the exact match is already there
-            if let Some(exact_node) =
-                routing_table.node_in_bucket(bucket_idx as usize, target_node_id)
-            {
-                // {"t":"aa", "y":"r", "r": {"id":"0123456789abcdefghij", "nodes": "def456..."}}
-                k_closest_nodes.push(exact_node.get_compact_format())
-            } else {
-                // 3. if not enough, move on to i + 1 bucket and wrap around if needed
-                let original_bucket = bucket_idx;
-                loop {
-                    // append new list of bucket+i
-                    for node in routing_table.buckets[bucket_idx as usize].iter() {
-                        k_closest_nodes.push(node.get_compact_format());
-                    }
+            let compact_node_info = get_nodes(routing_table, source_node, target_node_id);
 
-                    bucket_idx += 1;
-                    bucket_idx %= 160;
-
-                    if bucket_idx == original_bucket {
-                        break;
-                    }
-                }
-                // 4. collect the k elements and return
-                if k_closest_nodes.len() >= K {
-                    k_closest_nodes.truncate(K);
-                }
-            }
-
-            let compact_node_info = k_closest_nodes.concat();
             let mut return_values = HashMap::new();
-            return_values.insert("id".into(), routing_table.my_id.to_string());
+            return_values.insert("id".into(), routing_table.my_node.id.to_string());
             return_values.insert("nodes".into(), compact_node_info);
 
             let response = KrpcSuccessResponse {
@@ -324,11 +322,121 @@ async fn handle_krpc_call(
             socket.send_to(&response, addr).await.unwrap();
         }
         "get_peers" => {
+            let info_hash = query.a.get("info_hash").unwrap();
+            let peers = peer_store.get(info_hash);
 
+            let mut return_values = HashMap::new();
+            return_values.insert("id".into(), routing_table.my_node.id.to_string());
+
+            if let Some(peers) = peers {
+                // values
+                let values = peers
+                    .iter()
+                    .map(|peer| peer.get_peer_compact_format())
+                    .collect::<Vec<String>>()
+                    .concat();
+                return_values.insert("values".into(), values); // this won't work rn
+            } else {
+                // nodes
+                let source_id = query.a.get("id").unwrap().parse().unwrap();
+                let source_node = Node {
+                    id: source_id,
+                    ip: addr.ip(),
+                    port: addr.port(),
+                };
+                let compact_node_info =
+                    get_nodes(routing_table, source_node, info_hash.parse().unwrap());
+                return_values.insert("nodes".into(), compact_node_info);
+            }
+
+            // generate token
+            // hash ip + secret
+            let mut hasher = Sha1::new();
+            if let IpAddr::V4(v4addr) = addr.ip() {
+                hasher.update(v4addr.octets());
+            }
+            hasher.update(*routing_table.secret.lock().unwrap());
+            let token = format!("{:x}", hasher.finalize());
+
+            return_values.insert("token".into(), token);
+
+            let response = KrpcSuccessResponse {
+                y: "r".into(),
+                t: query.t,
+                r: return_values,
+            };
+            let response = serde_bencode::to_bytes(&response).unwrap();
+            socket.send_to(&response, addr).await.unwrap();
         }
-        "announce_peer" => {}
+        "announce_peer" => {
+            let querying_ip = query.a.get("id").unwrap();
+            let info_hash = query.a.get("info_hash").unwrap();
+            let port = query.a.get("port").unwrap();
+            let is_implied = query.a.get("implied_port").unwrap();
+            let token = query.a.get("token").unwrap();
+
+            let mut hasher = Sha1::new();
+            if let IpAddr::V4(querying_ip) = addr.ip() {
+                hasher.update(querying_ip.octets());
+            }
+            hasher.update(*routing_table.secret.lock().unwrap());
+
+            let target_token = format!("{:x}", hasher.finalize());
+
+            if (token.to_string() != target_token) {
+                // send failure message
+            }
+
+            let mut return_values: HashMap<String, String> = HashMap::new();
+            return_values.insert("id".to_string(), routing_table.my_node.ip.to_string());
+
+            let response = KrpcSuccessResponse {
+                y: "r".into(),
+                t: query.t,
+                r: return_values,
+            };
+
+            let response = serde_bencode::to_bytes(&response).unwrap();
+            socket.send_to(&response, addr).await.unwrap();
+        }
         _ => {}
-    };
+    }
+}
+
+fn get_nodes(routing_table: &mut RoutingTable, source_node: Node, target_node_id: u32) -> String {
+    let mut k_closest_nodes = vec![];
+    // 1. update source_id into routing table
+    routing_table.upsert_node(source_node);
+    // 2. find the initial k-bucket
+    let mut bucket_idx = routing_table.find_bucket_idx(target_node_id);
+    // 2.5. Check if the exact match is already there
+    if let Some(exact_node) = routing_table.node_in_bucket(bucket_idx as usize, target_node_id) {
+        // {"t":"aa", "y":"r", "r": {"id":"0123456789abcdefghij", "nodes": "def456..."}}
+        k_closest_nodes.push(exact_node.get_node_compact_format())
+    } else {
+        // 3. if not enough, move on to i + 1 bucket and wrap around if needed
+        let original_bucket = bucket_idx;
+        loop {
+            // append new list of bucket+i
+            for node in routing_table.buckets[bucket_idx as usize].iter() {
+                k_closest_nodes.push(node.get_node_compact_format());
+            }
+
+            bucket_idx += 1;
+            bucket_idx %= 160;
+
+            if bucket_idx == original_bucket {
+                break;
+            }
+        }
+        // 4. collect the k elements and return
+        if k_closest_nodes.len() >= K {
+            k_closest_nodes.truncate(K);
+        }
+    }
+
+    let compact_node_info = k_closest_nodes.concat();
+    compact_node_info
 }
 
 async fn send_ping(socket: &UdpSocket, addr: &str) {
@@ -355,10 +463,10 @@ async fn send_ping(socket: &UdpSocket, addr: &str) {
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    port: u16,
+    udp_port: u16,
 
-    #[arg(short = 't', long = "test")]
-    is_testing: bool,
+    #[arg(short, long)]
+    tcp_port: Option<u16>,
 
     #[arg(long)]
     bootstrap: Option<String>,
@@ -381,21 +489,71 @@ async fn main() {
     let our_node = Node {
         id,
         ip,
-        port: args.port,
+        port: args.udp_port,
     };
 
-    let socket = UdpSocket::bind(format!("127.0.0.1:{}", args.port))
+    let secret = Arc::new(Mutex::new(gen_secret()));
+
+    let socket = UdpSocket::bind(format!("127.0.0.1:{}", args.udp_port))
         .await
         .unwrap();
     println!("Started DHT node on {:#?}", our_node);
 
-    let mut routing_table = RoutingTable::new(id);
+    let mut routing_table = RoutingTable::new(our_node, Arc::clone(&secret));
+
+    let secret_clone = Arc::clone(&secret);
+    tokio::spawn(async move {
+        loop {
+            sleep(Duration::from_secs(600)).await;
+            let mut sec = secret_clone.lock().unwrap();
+            *sec = gen_secret();
+        }
+    });
+
     let mut peer_store: HashMap<String, Vec<Node>> = HashMap::new();
 
-    if args.is_testing {
-        send_ping(&socket, "127.0.0.1:8080").await;
-    }
+    // setup tcp server for external DHT interface
+    if let Some(tcp_port) = args.tcp_port {
+        tokio::spawn(async move {
+            let listener = TcpListener::bind(format!("127.0.0.1:{}", tcp_port))
+                .await
+                .unwrap();
 
+            loop {
+                let (mut socket, addr) = listener.accept().await.unwrap();
+                let (reader, mut writer) = socket.split();
+                let mut reader = BufReader::new(reader);
+                let mut buffer = String::new();
+
+                loop {
+                    match reader.read_line(&mut buffer).await {
+                        Ok(0) => {
+                            println!("Connection closed");
+                            break;
+                        }
+                        Ok(_) => {
+                            match buffer.trim_end().split_once(" ") {
+                                Some(("GET", info_hash)) => {}
+                                Some(("PUT", key_value)) => {}
+                                Some(("PING", address)) => {}
+                                _ => {}
+                            }
+                            println!("Received: {}", buffer.trim_end());
+                            if let Err(e) = writer.write_all(buffer.as_bytes()).await {
+                                eprintln!("Failed to write to socket; err = {:?}", e);
+                                break;
+                            }
+                            buffer.clear();
+                        }
+                        Err(e) => {
+                            eprintln!("Failed to read from socket; err = {:?}", e);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
     if let Some(bootstrap) = args.bootstrap {
         // 1. initialize k-bucket with another known node
         // 2. run find_nodes on itself to fill k-bucket table
